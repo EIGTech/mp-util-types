ef02cc659425609165189c5db56539df0d95375e
Andrew McCall <amccall@eigholdingsinc.com>
2020-11-19 14:27:43 -0800

    Export RM types

--- index.d.ts
+++ index.d.ts
@@ -3,5 +3,7 @@ export * from "./serviceAreas"
 export * from "./policies"
 export * from "./claimsService"
 export * from "./claims"
+export * from "./documents"
+export * from "./restorationManager"
 
 export as namespace MappingPlatform
